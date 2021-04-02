dfb17fd60c7591cbeea39dbc14b4f00177cc06d5
unknown <igr-kndratv@ukr.net>
2021-04-02 19:23:50 +0300

    'Media4K'

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
         infinite: true,
         speed: 700,
         fade: true,
-        zIndex: 0,
+        zIndex: 1,
         autoplay: true,
+        centerMode:true,
         appendArrows:$('.slick-arrow_block'),
         cssEase: 'linear'
       });
